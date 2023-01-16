7eb089d12fba5837624c5eee592dc9ceb97bdb65
NotZarifGithub <flacko.programming@gmail.com>
2023-01-17 02:47:09 +0800

    object exercise

---
+++ ZeroToMastery-Web Development/javascript-exercises/exercise-7.js
@@ -0,0 +1,38 @@
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+
+{
+    username: "zarif",
+    password: "123"
+
+};
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+
+let userData = [
+    {
+        username: "Zarif",
+        password: "123"
+    }
+
+];
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+
+let newsFeed = [
+    {
+        username: "Zarif",
+        timeline: "into coding at the moment"
+    },
+
+    {
+        username: "Rauf",
+        timeline: "I'm bored"
+    },
+
+    {
+        username: "Alie",
+        password: "Hello, world!"
+    }
+]
